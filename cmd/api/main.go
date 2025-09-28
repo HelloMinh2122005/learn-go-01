143b27a3a08112d91adc592061fd696c039e127d
Minh dep trai <phandinhminh48@gmail.com>
2025-09-28 15:14:20 +0700

    added swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,8 +7,25 @@ import (
 	services "minh.com/go-rest-gin-3/internal/services/implementations"
 
 	gin "github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "minh.com/go-rest-gin-3/docs" // Import to embed Swagger docs
 )
 
+// @title test API
+// @version 1.0
+// @description This is a sample server for a test service.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8089
 func main() {
 	userRepo := repositories.NewUserRepository()
 	// TODO: delete this shit
@@ -21,6 +38,11 @@ func main() {
 
 	// Đăng ký routes
 	routes.RegisterUserRoutes(router, userHandler)
+
+	// Đăng ký route swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
+		ginSwagger.URL("http://localhost:8089/swagger/doc.json"),
+	))
 
 	// Chạy server
 	router.Run(":8089")
